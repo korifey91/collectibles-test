3755284dcd9801bddc95151485400ff46c818b52
kirill <korifey91@gmail.com>
2023-05-08 21:04:33 +0300

    Chore: setup font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
 import { Provider } from 'react-redux';
 
 import type { AppProps } from 'next/app';
 
 import store from '../store';
 
+const inter = Inter({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Provider store={store}><Component {...pageProps} /></Provider>;
+  return (
+    <Provider store={store}>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </Provider>
+  );
 }
