8011a059f231d1808beaaecdac5559bcd09bb668
kirill <korifey91@gmail.com>
2023-05-09 14:09:47 +0300

    Feat: app redux selector and dispatch hooks

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,5 @@
+import { TypedUseSelectorHook, useSelector } from 'react-redux';
+
+import { AppState } from '@src/store';
+
+export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
