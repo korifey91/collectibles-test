16c2ffa3324fc8feeb11a39caff37aa637b0520e
kirill <korifey91@gmail.com>
2023-05-10 11:14:26 +0300

    Feat: wrap header component in memo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import CollectbaseLogo from '@public/collectbase-logo.svg';
 
@@ -12,4 +12,4 @@ function Header() {
   );
 }
 
-export default Header;
+export default memo(Header);
