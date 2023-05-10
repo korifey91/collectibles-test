9e5c6df5d38825434a3e054ea4c5cdcfc22cc707
kirill <korifey91@gmail.com>
2023-05-10 15:05:47 +0300

    Feat[search]: store query in URL and restore it on initial render

--- src/features/search/components/CardsSearchInput/CardsSearchInput.tsx
+++ src/features/search/components/CardsSearchInput/CardsSearchInput.tsx
@@ -1,19 +1,25 @@
+import { useRouter } from 'next/router';
 import React, {
   ChangeEventHandler, memo, useCallback, useDeferredValue, useState,
 } from 'react';
 import { useUpdateEffect } from 'react-use';
 
 import SearchInput from '@components/SearchInput';
+import { getQuery } from '@features/search/search.selectors';
 import { setQuery as setStoreQuery } from '@features/search/search.slice';
-import { useAppDispatch } from '@src/hooks';
+import { useAppDispatch, useAppSelector } from '@src/hooks';
 
 import {
   CardsSearchInputContainer,
 } from './CardsSearchInput.styled';
 
 function CardsSearchInput() {
+  const router = useRouter();
+
   const dispatch = useAppDispatch();
-  const [query, setQuery] = useState('');
+  const initialQuery = useAppSelector(getQuery);
+
+  const [query, setQuery] = useState(initialQuery);
 
   const deferredQuery = useDeferredValue(query);
 
@@ -25,7 +31,21 @@ function CardsSearchInput() {
   );
 
   useUpdateEffect(() => {
-    dispatch(setStoreQuery(deferredQuery.trim()));
+    const trimmedQuery = deferredQuery.trim();
+    dispatch(setStoreQuery(trimmedQuery));
+
+    const searchParams = new URLSearchParams('');
+    searchParams.set('query', trimmedQuery);
+
+    const urlToReplace = trimmedQuery
+      ? `${window.location.pathname}?${searchParams}`
+      : window.location.pathname;
+
+    router.replace(
+      urlToReplace,
+      undefined,
+      { shallow: true },
+    );
   }, [deferredQuery]);
 
   return (
