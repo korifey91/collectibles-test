0297b50b3d900bdcea1bfa2f2a81724fa79a5704
kirill <korifey91@gmail.com>
2023-05-10 13:23:51 +0300

    Feat[CardsSearchInput]: correct position

---
+++ src/features/search/components/CardsSearchInput/CardsSearchInput.styled.ts
@@ -0,0 +1,5 @@
+import { styled } from '@src/styles/styled';
+
+export const CardsSearchInputContainer = styled('div')`
+  transform: translateY(-50%);
+`;
--- src/features/search/components/CardsSearchInput/CardsSearchInput.tsx
+++ src/features/search/components/CardsSearchInput/CardsSearchInput.tsx
@@ -7,6 +7,10 @@ import SearchInput from '@components/SearchInput';
 import { setQuery as setStoreQuery } from '@features/search/search.slice';
 import { useAppDispatch } from '@src/hooks';
 
+import {
+  CardsSearchInputContainer,
+} from './CardsSearchInput.styled';
+
 function CardsSearchInput() {
   const dispatch = useAppDispatch();
   const [query, setQuery] = useState('');
@@ -25,11 +29,13 @@ function CardsSearchInput() {
   }, [deferredQuery]);
 
   return (
-    <SearchInput
-      placeholder="What do you want to find?"
-      value={query}
-      onChange={handleSearch}
-    />
+    <CardsSearchInputContainer>
+      <SearchInput
+        placeholder="What do you want to find?"
+        value={query}
+        onChange={handleSearch}
+      />
+    </CardsSearchInputContainer>
   );
 }
 
